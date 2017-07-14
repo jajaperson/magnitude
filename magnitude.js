b81d9a77a402859721f47ef11ab45202c9451a39
James Jensen <jamesarthurjensen@gmail.com>
2017-07-14 15:15:15 +0800

    Added Inherited Attributes
    * Removed `console.log()`
    * Added to inherited attributes
      * `class`
      * `style`

--- magnitude.js
+++ magnitude.js
@@ -3,9 +3,7 @@ $('[magnitude]').each(function(){
 
   $.get(el.src, function(doc){
     var svg = $(doc).find("svg").attr(inheritedAttributes());
-    $(svg).attr(inheritedAttributes(el))
-
-    console.log(inheritedAttributes(el))
+    $(svg).attr(inheritedAttributes(el));
 
     $(el).replaceWith(svg);
   }, "xml");
@@ -16,13 +14,15 @@ function inheritedAttributes(el){
 
   //Inherited Attributes
   ob.id = $(el).attr("id");
+  ob.class = $(el).attr("class");
   ob.source = $(el).attr("src");
   ob.magnitude = $(el).attr("magnitude");
   ob.width = $(el).attr("width");
   ob.height = $(el).attr("height");
+  ob.style = $(el).attr("height");
 
   //Special Attributes for Magnitude functionality
-  ob.preserveAspectRatio = "none"
+  ob.preserveAspectRatio = "none";
 
-  return ob
+  return ob;
 };
